485315763392b596aef6ec4ed705abfe5a0303dd
romoguill <romoguill@gmail.com>
2025-05-27 08:15:22 -0300

    Added github workflow

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
 import CreateProduct from './create-products/create-product';
 import Products from './products/products';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <>
